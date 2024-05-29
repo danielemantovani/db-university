4d767fbc76c64871bdb8820df7d010b43e3e7789
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-29 16:31:31 +0200

    feat: query 4-5

--- queries.sql
+++ queries.sql
@@ -11,7 +11,14 @@ SELECT *
 FROM `students` 
 WHERE `date_of_birth` <= DATE_SUB(CURRENT_DATE(),INTERVAL 30 YEAR);
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses` 
+WHERE `year` = 1 AND `period` = "I semestre";
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * 
+FROM `exams` 
+WHERE hour > "14:00" 
+AND `date` = "2020-06-20";
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
